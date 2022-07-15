ddee8ff848fd5e21114703037b0ccff2edc9d84e
paolosantarsiero <paolosantarsiero9@gmail.com>
2022-07-15 17:47:41 +0200

    feat: add vector icons

--- template/src/Navigators/Tab.tsx
+++ template/src/Navigators/Tab.tsx
@@ -2,13 +2,22 @@ import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { MapContainer } from '@/Containers'
 import ListRoutesNavigator from './ListRoutes'
+import Icon from 'react-native-vector-icons/Ionicons'
 
 const Tab = createBottomTabNavigator()
 
 const TabNavigator = () => {
   return (
     <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="List" component={ListRoutesNavigator} />
+      <Tab.Screen
+        name="List"
+        component={ListRoutesNavigator}
+        options={{
+          tabBarIcon: () => (
+            <Icon name="ios-person" size={30} color="#4F8EF7" />
+          ),
+        }}
+      />
       <Tab.Screen name="Map" component={MapContainer} />
     </Tab.Navigator>
   )
